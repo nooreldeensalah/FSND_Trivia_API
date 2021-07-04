a9090eb3fb898d22099b8df6174bc8ea37eafc87
Noor Eldeen Salah <m-2123325228@s-mu.edu.eg>
2021-07-04 04:39:16 +0200

    Update pip packages to latest versions and export to requirements.txt. Configure database connection strings for a local database.

--- backend/test_flaskr.py
+++ backend/test_flaskr.py
@@ -13,9 +13,10 @@ class TriviaTestCase(unittest.TestCase):
     def setUp(self):
         """Define test variables and initialize app."""
         self.app = create_app()
-        self.client = self.app.test_client
-        self.database_name = "trivia_test"
-        self.database_path = "postgres://{}/{}".format('localhost:5432', self.database_name)
+        self.client = self.app.test_client()
+        self.database_path = (
+            "postgresql://postgres:mypassword@localhost:5432/trivia_test"
+        )
         setup_db(self.app, self.database_path)
 
         # binds the app to the current context
@@ -24,7 +25,7 @@ class TriviaTestCase(unittest.TestCase):
             self.db.init_app(self.app)
             # create all tables
             self.db.create_all()
-    
+
     def tearDown(self):
         """Executed after reach test"""
         pass
