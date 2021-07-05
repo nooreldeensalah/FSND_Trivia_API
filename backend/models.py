797b07bb9a117a44924c95374aa94379ad2900ab
Noor Eldeen Salah <m-2123325228@s-mu.edu.eg>
2021-07-05 22:55:33 +0200

    Use environment variables for database connections

--- backend/models.py
+++ backend/models.py
@@ -3,7 +3,11 @@ from sqlalchemy import Column, String, Integer, create_engine
 from flask_sqlalchemy import SQLAlchemy
 import json
 
-database_path = "postgresql://postgres:mypassword@localhost:5432/trivia"
+DB_HOST = os.getenv("DB_HOST", "localhost:5432")
+DB_USER = os.getenv("DB_USER", "postgres")
+DB_PASSWORD = os.getenv("DB_PASSWORD", "mypassword")
+DB_NAME = os.getenv("DB_NAME", "trivia")
+database_path = f"postgresql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}/{DB_NAME}"
 
 db = SQLAlchemy()
 
--- backend/test_flaskr.py
+++ backend/test_flaskr.py
@@ -6,6 +6,11 @@ from flask_sqlalchemy import SQLAlchemy
 from flaskr import create_app
 from models import setup_db, Question, Category
 
+DB_HOST = os.getenv("DB_HOST", "localhost:5432")
+DB_USER = os.getenv("DB_USER", "postgres")
+DB_PASSWORD = os.getenv("DB_PASSWORD", "mypassword")
+DB_NAME = os.getenv("DB_NAME", "trivia_test")
+
 
 class TriviaTestCase(unittest.TestCase):
     """This class represents the trivia test case"""
@@ -14,9 +19,7 @@ class TriviaTestCase(unittest.TestCase):
         """Define test variables and initialize app."""
         self.app = create_app()
         self.client = self.app.test_client()
-        self.database_path = (
-            "postgresql://postgres:mypassword@localhost:5432/trivia_test"
-        )
+        self.database_path = f"postgresql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}/{DB_NAME}"
         setup_db(self.app, self.database_path)
 
         # binds the app to the current context
